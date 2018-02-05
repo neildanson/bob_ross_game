cabdefebb6f868f121aae8347608c3da1c1ebaba
Neil Danson <neil.danson@gmail.com>
2018-02-05 19:58:30 +0000

    no audio

--- src/engine/audio.rs
+++ src/engine/audio.rs
@@ -1,31 +1,29 @@
-extern crate rodio;
+//extern crate rodio;
 
 use std::io::BufReader;
 use std::fs::File;
-use self::rodio::Source;
+//use self::rodio::Source;
 
 
 pub struct Audio {
-    sink : rodio::Sink,
+    //sink : rodio::Sink,
 }
 
 impl Audio {
     pub fn new(filename : &str) -> Audio {
-        let endpoint = rodio::get_default_endpoint().unwrap();
+        /*let endpoint = rodio::get_default_endpoint().unwrap();
         let sink = rodio::Sink::new(&endpoint);
 
         let file = File::open(filename).unwrap();
         let decoder = rodio::Decoder::new(BufReader::new(file)).unwrap();
         let decoder = decoder.repeat_infinite();
-        sink.append(decoder);
-        Audio { sink : sink }
+        sink.append(decoder);*/
+        Audio { }
     }
 
     pub fn play(&self) {
-        self.sink.play()
     }
 
     pub fn pause(&self) {
-        self.sink.pause()
     }
 }
