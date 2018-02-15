eaa8d8368463ef33b6f0fea60a5f84c2d30d5a37
Neil Danson <neil.danson@gmail.com>
2018-02-15 20:10:07 +0000

    sensible number of squirrels

---
+++ src/constants.rs
@@ -0,0 +1,15 @@
+pub const SCREEN_WIDTH: u32 = 320;
+pub const SCREEN_HEIGHT: u32 = 240;
+
+pub const PLAYER_SPEED: i32 = 2;
+
+pub const SQUIRREL_SPEED: i32 = 1;
+pub const NUM_SQUIRRELS: u32 = 100;
+
+pub const MAP_SIZE: i32 = 50;
+pub const TILE_SIZE: i32 = 16;
+pub const MAP_SIZE_SCALED: i32 = MAP_SIZE * TILE_SIZE;
+
+pub const PLAYER_LAYER: i32 = 5;
+pub const ENEMY_LAYER: i32 = 3;
+pub const BACKGROUND_LAYER: i32 = 1;
